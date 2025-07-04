70e283e75bfa0397004ac0b96725fae0f71a3db8
PythOn <pallavs2020@gmail.com>
2025-07-04 14:03:04 +0530

    Redeploy work

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY ./app.py .
 
-CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["sh", "-c", "uvicorn app:app --host 0.0.0.0 --port $PORT"]
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
+import uvicorn
 from fastapi import FastAPI, UploadFile, File
 import whisper
 import shutil
 
 app = FastAPI()
-model = whisper.load_model("large")  # You can choose 'tiny', 'base', 'small', 'medium', 'large'
+model = whisper.load_model("large")  # or your choice
 
 @app.post("/transcribe")
 async def transcribe_audio(file: UploadFile = File(...)):
@@ -13,3 +15,7 @@ async def transcribe_audio(file: UploadFile = File(...)):
 
     result = model.transcribe(temp_file)
     return {"text": result["text"]}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
